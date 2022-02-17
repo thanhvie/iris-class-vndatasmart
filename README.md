153fbf69918ee1fed69179ef0961ff71b1219323
Huynh Thanh Viet <huynhthviet@gmail.com>
2022-02-17 14:01:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# iris-class-vndatasmart
+# iris-class-vndatasmart
---
+++ model.py
@@ -0,0 +1,17 @@
+import numpy as np
+import joblib
+
+iris_classes = ['Setosa','Versicolor','Virginica']
+
+filename = 'iris_model.pkl'
+multi_model = joblib.load(filename)
+
+# Function for classification based on inputs
+def classify(a, b, c, d):
+    client_input = np.array([[a, b, c, d]]) # Convert to numpy array
+    iris_pred = multi_model.predict(client_input)[0] # Retrieve from dictionary
+    prediction = 'Predicted class is ' + iris_classes[iris_pred]
+    print(prediction)
+    return prediction # Return the prediction
+
+#classify(1,2,3,4)
